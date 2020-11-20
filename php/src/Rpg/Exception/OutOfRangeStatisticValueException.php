412017155e6eb23b4d93f28f00e09f42e9e128df
Ludovic FLEURY <ludovic.fleury@golem.ai>
2020-11-20 08:34:27 +0100

    Add RPG statistics

---
+++ php/src/Rpg/Exception/OutOfRangeStatisticValueException.php
@@ -0,0 +1,25 @@
+<?php
+declare(strict_types=1);
+
+namespace Rpg\Exception;
+
+use DomainException;
+
+class OutOfRangeStatisticValueException extends DomainException
+{
+    public function __construct(string $type, string $name, int $requirement, int $value)
+    {
+        $lower = '%s "%s" (%d) is under the minimum requirement of %d';
+        $greater = '%s "%s" (%d) exceed the maximum requirement of %d';
+
+        $message = sprintf(
+            $value < $requirement ? $lower : $greater,
+            ucfirst($type),
+            $name,
+            $value,
+            $requirement
+        );
+
+        parent::__construct($message);
+    }
+}
