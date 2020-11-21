217cd1069421ef8a9bb534c8264462bab646d88a
Ludovic FLEURY <ludovic.fleury@golem.ai>
2020-11-22 00:14:59 +0100

    Add Black Flag character

---
+++ php/src/BlackFlag/Character/Character.php
@@ -0,0 +1,59 @@
+<?php
+declare(strict_types=1);
+
+namespace BlackFlag\Character;
+
+use BlackFlag\Character\Event\CharacterCreated;
+use EventSourcing\AggregateRoot;
+use Ramsey\Uuid\Uuid;
+
+final class Character extends AggregateRoot
+{
+    private string $firstname;
+
+    private string $lastname;
+
+    private string $nickname;
+
+    private int $age;
+
+    private bool $gender;
+
+    private Attributes $attributes;
+
+    /**
+     * @param array<string, int>  $attributes
+     */
+    public static function create(
+        string $firstname,
+        string $lastname,
+        string $nickname,
+        int $age,
+        bool $gender,
+        array $attributes
+    ): Character {
+        $character = new self(Uuid::uuid4());
+
+        $event = new CharacterCreated(
+            $firstname,
+            $lastname,
+            $nickname,
+            $age,
+            $gender,
+            $attributes
+        );
+        $character->apply($event);
+
+        return $character;
+    }
+
+    protected function applyCharacterCreated(CharacterCreated $event): void
+    {
+        $this->firstname = $event->getFirstname();
+        $this->lastname = $event->getLastname();
+        $this->nickname = $event->getNickname();
+        $this->age = $event->getAge();
+        $this->gender = $event->getGender();
+        $this->attributes = new Attributes($this);
+    }
+}
---
+++ php/src/BlackFlag/Character/Exception/AttributesMissingException.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+
+namespace BlackFlag\Character\Exception;
+
+use DomainException;
+
+class AttributesMissingException extends DomainException
+{
+}
